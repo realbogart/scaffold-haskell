af59d52f539545a9e005fc16b3af257db039570c
Johan Yngman <johan.yngman@gmail.com>
2025-06-26 13:31:22 +0000

    A couple of bench examples are enough

--- templates/bench/Main.hs
+++ templates/bench/Main.hs
@@ -8,15 +8,6 @@ main :: IO ()
 main = defaultMain
   [ bgroup "String operations"
     [ bench "reverse short string" $ whnf reverse "hello"
-    , bench "reverse long string" $ whnf reverse (replicate 1000 'a')
     , bench "concatenation" $ whnf (++ "world") "hello "
-    ]
-  , bgroup "List operations"
-    [ bench "length [1..100]" $ whnf length [1..100 :: Int]
-    , bench "sum [1..100]" $ whnf sum [1..100 :: Int]
-    , bench "reverse [1..100]" $ whnf reverse [1..100 :: Int]
-    ]
-  , bgroup "IO operations"
-    [ bench "putStrLn simulation" $ nfIO (return () :: IO ())
     ]
   ]
